67563aebd9dade42e45ebddc5074d6b2ff8de4a6
kaleab <tadewoskaleab@gmail.com>
2020-05-30 12:52:49 -0500

    allow cross origin

--- src/main/java/rc/bootsecurity/controller/PublicRestApiController.java
+++ src/main/java/rc/bootsecurity/controller/PublicRestApiController.java
@@ -2,6 +2,7 @@ package rc.bootsecurity.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import rc.bootsecurity.model.User;
 
 @RestController
 @RequestMapping("api/public")
+@CrossOrigin
 public class PublicRestApiController {
 	
 	public UserRepository userRepository;
