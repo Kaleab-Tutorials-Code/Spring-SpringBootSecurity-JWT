858cf66b3e525599c5935c18d25dde11121abd11
kaleab <tadewoskaleab@gmail.com>
2020-05-31 18:33:40 -0500

    At this moment every both JWTAuthentication and JWTAuthorization filters are configured. so the remaining task is to make spring configuration. - read comments for further detail

--- src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
+++ src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
@@ -2,24 +2,33 @@ package rc.bootsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import rc.bootsecurity.db.UserRepository;
+
+//1. Here lets actually make use of our JWTAuthentication and JWTAuthorization filters and integrate them
+//into spring security pipeline.
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     
 	//I need this because it is the one provides me userPricipalDetailSerivce which going to be used by DAOAuthentication Provider.
 	private UserPrincipalDetailsService userPrincipalDetailsService;
+	//userrepository is needed to actually get user data
+	private UserRepository userRepository;
 
     public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
         this.userPrincipalDetailsService = userPrincipalDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -30,8 +39,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	http
+        //here// remove csrf(cross site request forgery) and state in session because in jwt we do not need them
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        // add jwt filters (1. authentication, 2. authorization)
+        //note that : order matters here between authentication and authorization filters
+        .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+        .addFilter(new JwtAuthorizationFilter(authenticationManager(),  this.userRepository))
         .authorizeRequests()
-        .antMatchers("*").permitAll();
+        //here// configure access rules
+        .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .antMatchers("/api/public/management/*").hasRole("MANAGER")
+        .antMatchers("/api/public/admin/*").hasRole("ADMIN")
+        .anyRequest().authenticated();
     }
 
     //this is authentication provider configuration. everything UserpricipalDetailService is done for it.
